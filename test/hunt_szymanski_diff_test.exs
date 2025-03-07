b5d0c8153283205f4025ac280acc2c3942078103
Daniel Reszka <dannn.r@gmail.com>
2025-03-07 15:25:09 +0100

    add HSDiff + HSDiffTest

--- test/hunt_szymanski_diff_test.exs
+++ test/hunt_szymanski_diff_test.exs
@@ -1,8 +1,36 @@
-defmodule HuntSzymanskiDiffTest do
+defmodule HSDiffTest do
   use ExUnit.Case
-  doctest HuntSzymanskiDiff
 
-  test "greets the world" do
-    assert HuntSzymanskiDiff.hello() == :world
+  test "diff with small localized changes in large text" do
+    left = [
+      "Line A",
+      "Line B",
+      "Line C",
+      "Line D",
+      "Line E",
+      "Line F"
+    ]
+
+    right = [
+      "Line A",
+      "Line B changed",
+      "Line C",
+      "Line D",
+      "Line E new",
+      "Line F"
+    ]
+
+    diff_result = HSDiff.diff(left, right)
+
+    assert diff_result == [
+      eq: ["Line A"],
+      del: ["Line B"],
+      ins: ["Line B changed"],
+      eq: ["Line C"],
+      eq: ["Line D"],
+      del: ["Line E"],
+      ins: ["Line E new"],
+      eq: ["Line F"]
+    ]
   end
 end
