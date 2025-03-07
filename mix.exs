609441be119b5fc0b3bef91de478de233113f179
Daniel Reszka <dannn.r@gmail.com>
2025-03-07 17:47:39 +0100

    fix mix.exs for publishing on hex.pm

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule HuntSzymanskiDiff.MixProject do
+defmodule HSDiff.MixProject do
   use Mix.Project
 
   def project do
@@ -7,7 +7,9 @@ defmodule HuntSzymanskiDiff.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -21,16 +23,15 @@ defmodule HuntSzymanskiDiff.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
   defp description() do
-    """
-    A port of the Hunt-Szymanski diff algorithm to Elixir, with a focus on performance and memory efficiency.
-    """
+    "A port of the Hunt-Szymanski diff algorithm to Elixir, with a focus on performance and memory efficiency."
   end
 
   defp package() do
